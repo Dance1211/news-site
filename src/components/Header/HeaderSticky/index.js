1671fc37d9050c492c5f9c670171cb6b60d54577
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-08 13:13:47 +0000

    Random fixes

--- src/components/Header/HeaderSticky/index.js
+++ src/components/Header/HeaderSticky/index.js
@@ -4,7 +4,7 @@ import './styles.css';
 export function HeaderSticky({toggleIsNavOpen}) {
   return <div className="HeaderSticky">
     <Link to={'/'} className="HeaderSticky__link">
-      <h1 className="HeaderStick__title">NC-NEWS</h1>
+      <h1 className="HeaderSticky__title">NC-NEWS</h1>
     </Link>
     <button className="HeaderSticky__navButton" onClick={toggleIsNavOpen}>
       <i style={{ color: "white" }} className="fas fa-bars" />
