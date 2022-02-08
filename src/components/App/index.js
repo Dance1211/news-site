64eb498abf5df74c4ec5e1d8240510324c76514c
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-08 10:49:42 +0000

    Added simple footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../Header';
 import Home from '../pages/Home';
 import Article from '../pages/Article';
-import './styles.css';
 import Error from '../Error';
+import Footer from '../Footer';
+import './styles.css';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/t/:slug/:article_id" element={<Article />} />
           <Route path="*" element={<Error/>} />
         </Routes>
+        <Footer/>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import './styles.css';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <h2 className="Footer__siteName">NC-NEWS</h2>
+      <Links/>
+      <p className="Footer__copyright">© 2022 Joshua Banister </p>
+    </footer>
+  );
+}
+
+function Links() {
+  return (
+    <nav>
+      <Link to="/" className="Footer__link">Home</Link>
+    </nav>
+  );
+}
+
+export default Footer;
