d5f08ec0c9deda57e901184b752554257fb67df9
dance1211 <2436817+Dance1211@users.noreply.github.com>
2022-02-11 12:27:57 +0000

    Added some links to the nav bar

--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -4,16 +4,35 @@ import { AuthorCard } from '@components/AuthorCard';
 import UserContext from '@context/User';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
-function Nav({isNavOpen}) {
+function Nav({ isNavOpen }) {
   const { user } = useContext(UserContext)
   return (
     <nav className={`Nav ${isNavOpen && "Nav--open"}`}>
       <div className="Nav__contents">
-        <AuthorCard author={user}/>
+        <AuthorCard author={user} />
+        <hr />
+        <NavLinks username={user.username} />
       </div>
     </nav>
   );
+}
+
+function NavLinks({ username }) {
+  return (
+    <section>
+      <p>Links</p>
+      <ul>
+        <li>
+          <Link to="/">- Home</Link>
+        </li>
+        <li>
+          <Link to={`/u/${username}`}>- Profile</Link>
+        </li>
+      </ul>
+    </section>
+  )
 }
 
 export default Nav;
